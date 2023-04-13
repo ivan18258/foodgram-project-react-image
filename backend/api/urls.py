fe6238d906789211e11867d53e413ab55be11f27
ivanb 18258 <ivanbor2016@gmail.com>
2023-04-14 00:55:23 +0300

    1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,7 +16,7 @@ router = RuDefaultRouter()
 router.register('tags', TagViewSet, 'tags')
 router.register('ingredients', IngredientViewSet, 'ingredients')
 router.register('recipes', RecipeViewSet, 'recipes')
-router.register('users/', UserViewSet, 'users')
+router.register('users', UserViewSet, 'users')
 
 urlpatterns = (
     path('', include(router.urls)),
